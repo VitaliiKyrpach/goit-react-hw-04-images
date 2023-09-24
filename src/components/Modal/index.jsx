eccabe2efc8388c8f1463996eb3d374e76b9f480
VitaliiKyrpach <vitalij.9f0x9@gmail.com>
2023-09-24 14:32:24 +0300

    close modal on esc

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,4 +1,15 @@
+import { useEffect } from 'react';
+
 export const Modal = ({ image, toggle }) => {
+  useEffect(() => {
+    const handleEsc = e => {
+      if (e.code === 'Escape') toggle();
+    };
+    document.addEventListener('keydown', handleEsc);
+
+    return () => document.removeEventListener('keydown', handleEsc);
+  });
+
   return (
     <div className="Overlay" onClick={toggle}>
       <div className="Modal">
